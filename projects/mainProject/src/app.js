2d049c58b2dd3579c663b0f1b17c6ac5bd0dfb69
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-09-22 22:27:19 +0530

    added setup of cloudinary and multer

--- projects/mainProject/src/app.js
+++ projects/mainProject/src/app.js
@@ -12,4 +12,4 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-export default app
+export {app}
