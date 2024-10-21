17d4c6d4ef4c8a1cd9ec8536f9407395ed1907d7
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-10-21 20:49:09 +0530

    testing

--- projects/mainProject/src/controllers/comment.controller.js
+++ projects/mainProject/src/controllers/comment.controller.js
@@ -1,18 +1,31 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    if (!videoId || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const getAllComments = await Comment.aggregate([
+        {
+            $match: {
+                videoId,
+            }
+        }
+    ])
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    
 })
 
 const updateComment = asyncHandler(async (req, res) => {
